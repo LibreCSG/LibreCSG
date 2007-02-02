73ed3d541b4f039472a58ccc3ece9c2ea2ffaa40
Adam Kumpf <akumpf@gmail.com>
2007-02-02 20:29:29 +0000

    only very minor changes were made

--- ui/shells/MainAvoCADoShell.java
+++ ui/shells/MainAvoCADoShell.java
@@ -14,15 +14,15 @@ public class MainAvoCADoShell extends Shell{
 		super(display);
 		setupShell();
 		this.setText("avoCADo");
-		this.setSize(640, 480);		//TODO: set intial size to last known size
+		this.setSize(800, 600);		//TODO: set intial size to last known size
 		this.open();
 	}
 	
 	/**
 	 * yes, subclassing a shell is not so swell...
 	 * But it offers a nice level of abstraction
-	 * as long as long as routines within this
-	 * class make an effort to "play nice"
+	 * as long as routines within this class 
+	 * make an effort to "play nicely."
 	 */
 	protected void checkSubclass(){
 	}
@@ -30,7 +30,7 @@ public class MainAvoCADoShell extends Shell{
 	
 	/**
 	 * setup the shell with each of its key components
-	 *
+	 * (menubar, menuet, treeviewer, glview, quicksettings, etc.)
 	 */
 	void setupShell(){
 		//TODO: populate the main shell!
