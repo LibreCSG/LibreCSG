6bd5a018ad5b42038c18d98650db4d9dcaa1a631
Adam Kumpf <akumpf@gmail.com>
2007-02-02 21:54:47 +0000

    main shell layout of item regions

--- ui/shells/MainAvoCADoShell.java
+++ ui/shells/MainAvoCADoShell.java
@@ -1,39 +1,176 @@
 package ui.shells;
 
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.custom.SashForm;
+import org.eclipse.swt.events.MouseEvent;
+import org.eclipse.swt.events.MouseListener;
+import org.eclipse.swt.graphics.Color;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Menu;
+import org.eclipse.swt.widgets.MenuItem;
 import org.eclipse.swt.widgets.Shell;
 
+//
+//Copyright (C) 2007 avoCADo (Adam Kumpf creator)
+//This code is distributed under the terms of the 
+//GNU General Public License (GPL).
+//
+// This file is part of avoADo.
+//
+//AvoCADo is free software; you can redistribute it and/or modify
+//it under the terms of the GNU General Public License as published by
+//the Free Software Foundation; either version 2 of the License, or
+//(at your option) any later version.
+//
+//AvoCADo is distributed in the hope that it will be useful,
+//but WITHOUT ANY WARRANTY; without even the implied warranty of
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//GNU General Public License for more details.
+//
+//You should have received a copy of the GNU General Public License
+//along with AvoCADo; if not, write to the Free Software
+//Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+//
 
-public class MainAvoCADoShell extends Shell{
+/*
+* @author  Adam Kumpf
+* @created Feb. 2007
+*/
+public class MainAvoCADoShell{
 
+	Shell shell;
+	
+	SashForm comp2topSash;
+	
+	
 	/**
-	 * create the main avoCADo shell
+	 * create the main avoCADo shell and display it
 	 * @param display
 	 */
 	public MainAvoCADoShell(Display display){
-		super(display);
-		setupShell();
-		this.setText("avoCADo");
-		this.setSize(800, 600);		//TODO: set intial size to last known size
-		this.open();
+		shell = new Shell(display);
+		
+		setupShell(); 				// place components in the main avoCADo shell
+		
+		shell.setText("avoCADo");
+		shell.setSize(800, 600);	//TODO: set intial size to last known size
+		shell.open();
+		
+		// handle events while the shell is not disposed
+		while (!shell.isDisposed()) {
+			if (!display.readAndDispatch())
+				display.sleep();
+		}
 	}
 	
-	/**
-	 * yes, subclassing a shell is not so swell...
-	 * But it offers a nice level of abstraction
-	 * as long as routines within this class 
-	 * make an effort to "play nicely."
-	 */
-	protected void checkSubclass(){
-	}
-
 	
 	/**
 	 * setup the shell with each of its key components
 	 * (menubar, menuet, treeviewer, glview, quicksettings, etc.)
 	 */
 	void setupShell(){
-		//TODO: populate the main shell!
+		// --populate the main shell--	
+		
+		//
+		// give the shell a menubar (top)
+		//
+		Menu menu = new Menu(shell, SWT.BAR);
+		shell.setMenuBar(menu);
+		// TODO: populate the menubar
+		// HACK: just for now...
+		MenuItem miFile = new MenuItem(menu, SWT.CASCADE);
+		miFile.setText("&File");
+		MenuItem miEdit = new MenuItem(menu, SWT.CASCADE);
+		miEdit.setText("&Edit");
+		
+		//
+		// break the shell into two pieces (left/right)
+		//
+		GridLayout gl = new GridLayout();
+		gl.numColumns = 2;
+		gl.marginWidth = 0;		
+		gl.marginHeight = 0;
+		gl.marginWidth = 0;
+		gl.verticalSpacing = 0;
+		gl.horizontalSpacing = 0;
+		shell.setLayout(gl);
+		
+		//
+		// set the small left piece as the menuet
+		//
+		// TODO: use menuet instead of a dummy composite
+		Composite menuet = new Composite(shell, SWT.NONE);
+		menuet.setBackground(new Color(shell.getDisplay(), 100, 200, 100));
+		GridData gd0 = new GridData(GridData.FILL_VERTICAL);
+		gd0.grabExcessVerticalSpace = true;
+		gd0.widthHint = 65;
+		gd0.minimumWidth = 65;
+		menuet.setLayoutData(gd0);
+		
+		//
+		// break the right piece into a top and bottom
+		//
+		Composite comp2 = new Composite(shell, SWT.NONE);
+		GridData gd1 = new GridData(GridData.FILL_BOTH);
+		gd1.grabExcessHorizontalSpace = true;
+		gd1.grabExcessVerticalSpace = true;
+		comp2.setLayoutData(gd1);
+		comp2.setBackground(new Color(shell.getDisplay(), 100, 100, 200));		
+		
+		GridLayout gl2 = new GridLayout();
+		gl2.numColumns = 1;
+		gl2.marginWidth = 0;		
+		gl2.marginHeight = 0;
+		gl2.marginWidth = 0;
+		gl2.verticalSpacing = 0;
+		gl2.horizontalSpacing = 0;
+		comp2.setLayout(gl2);
+		
+		//
+		// break the top piece into two more pieces via a sash
+		//
+		comp2topSash = new SashForm(comp2, SWT.NONE);
+		comp2topSash.setBackground(new Color(shell.getDisplay(), 200, 100, 100));
+		comp2topSash.SASH_WIDTH = 5;		
+		GridData gd2top = new GridData(GridData.FILL_BOTH);
+		gd2top.grabExcessHorizontalSpace = true;
+		gd2top.grabExcessVerticalSpace = true;
+		comp2topSash.setLayoutData(gd2top);
+		
+		//
+		// large left piece is glview
+		//
+		// TODO: use glview instead of dummy composite
+		Composite comp3left = new Composite(comp2topSash, SWT.NONE);
+		comp3left.setBackground(new Color(shell.getDisplay(), 50, 50, 50));
+		
+		
+		//
+		// right piece is treeviewer
+		//
+		// TODO: use treeviewer instead of dummy composite
+		Composite comp3right = new Composite(comp2topSash, SWT.NONE);
+		comp3right.setBackground(new Color(shell.getDisplay(), 200, 200, 200));
+		
+		//
+		// now that items have been added to sash, set the weights.
+		// TODO: intelligently set sash weights
+		comp2topSash.setWeights(new int[] {5, 1});
+		
+		//
+		// remaining little section on the bottom is quicksetting bar
+		//
+		// TODO: use quicksetting instead of dummy composite
+		Composite comp2bot = new Composite(comp2, SWT.NONE);
+		comp2bot.setBackground(new Color(shell.getDisplay(), 150, 50, 150));
+		GridData gd2bot = new GridData(GridData.FILL_HORIZONTAL);
+		gd2bot.grabExcessHorizontalSpace = true;
+		gd2bot.heightHint = 25;
+		gd2bot.minimumHeight = 25;
+		comp2bot.setLayoutData(gd2bot);
 		
 	}
 	
