c185f5a0cf65532b1721ab872de098a5616b8fa3
Adam Kumpf <akumpf@gmail.com>
2007-02-07 04:37:06 +0000

    more tools (label/done)

---
+++ ui/tools/DD/Tool2DDoneInt.java
@@ -0,0 +1,58 @@
+package ui.tools.DD;
+
+import javax.media.opengl.GL;
+
+import backend.adt.ParamSet;
+import ui.tools.ToolInterface;
+
+
+//
+//Copyright (C) 2007 avoCADo (Adam Kumpf creator)
+//This code is distributed under the terms of the 
+//GNU General Public License (GPL).
+//
+//This file is part of avoADo.
+//
+//AvoCADo is free software; you can redistribute it and/or modify
+//it under the terms of the GNU General Public License as published by
+//the Free Software Foundation; either version 2 of the License, or
+//(at your option) any later version.
+//
+//AvoCADo is distributed in the hope that it will be useful,
+//but WITHOUT ANY WARRANTY; without even the implied warranty of
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//GNU General Public License for more details.
+//
+//You should have received a copy of the GNU General Public License
+//along with AvoCADo; if not, write to the Free Software
+//Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+//
+
+/*
+* @author  Adam Kumpf
+* @created Feb. 2007
+*/
+public class Tool2DDoneInt implements ToolInterface {
+	/**
+	 * All of the tool's main functionality
+	 * mouse handling, glView drawing, 
+	 * parameter storage, etc.
+	 *
+	 */
+	public Tool2DDoneInt(){
+	}
+	
+	
+	public void glMouseDown(double x, double y, double z, int mouseX, int mouseY) {
+	}
+
+	public void glMouseDrag(double x, double y, double z, int mouseX, int mouseY) {		
+	}
+
+	public void glMouseUp(double x, double y, double z, int mouseX, int mouseY) {
+	}
+
+	public void glDrawFeature(GL gl, ParamSet p) {
+		System.out.println("This is the 2D Done button.. it should never be drawn!");
+	}
+}
