4c04275cf8216d52b41b65b2dafc2ab5515642ac
Adam Kumpf <akumpf@gmail.com>
2007-02-08 05:22:19 +0000

    first pass at implementing the paramDialog.. but I think it needs reworked.. saving the code for now, but plan on changing a lot of the paramDialog implementation soon.

---
+++ ui/animator/Animator.java
@@ -0,0 +1,75 @@
+package ui.animator;
+
+import org.eclipse.swt.widgets.Display;
+
+
+//
+//Copyright (C) 2007 avoCADo (Adam Kumpf creator)
+//This code is distributed under the terms of the 
+//GNU General Public License (GPL).
+//
+//This file is part of avoADo.
+//
+//AvoCADo is free software; you can redistribute it and/or modify
+//it under the terms of the GNU General Public License as published by
+//the Free Software Foundation; either version 2 of the License, or
+//(at your option) any later version.
+//
+//AvoCADo is distributed in the hope that it will be useful,
+//but WITHOUT ANY WARRANTY; without even the implied warranty of
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//GNU General Public License for more details.
+//
+//You should have received a copy of the GNU General Public License
+//along with AvoCADo; if not, write to the Free Software
+//Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+//
+
+/*
+* @author  Adam Kumpf
+* @created Feb. 2007
+*/
+public abstract class Animator {
+
+	int mSec = 25;	// 40Hz animation framerate (ideally)
+	long timeBegin = 0L;
+	long duration  = 0L;
+	
+	public void animateForwards(long mSecDuration){
+		timeBegin = System.currentTimeMillis();
+		duration  = mSecDuration; 
+		
+		animatorTransition(0.0f);
+		new Runnable() {
+			public void run() {		
+				float percentComplete = (float)(System.currentTimeMillis()-timeBegin) / (float)duration;
+				if(percentComplete <= 1.0){
+					animatorTransition(percentComplete);
+					Display.getCurrent().timerExec(mSec,this);
+				}else{
+					animatorTransition(1.0f);
+				}
+			}
+		}.run();
+	}
+	
+	public void animateBackwards(long mSecDuration){
+		timeBegin = System.currentTimeMillis();
+		duration  = mSecDuration; 		
+		animatorTransition(1.0f);
+		new Runnable() {
+			public void run() {		
+				float percentComplete = (float)(System.currentTimeMillis()-timeBegin) / (float)duration;
+				if(percentComplete <= 1.0){
+					animatorTransition(1.0f - percentComplete);
+					Display.getCurrent().timerExec(mSec,this);
+				}else{
+					animatorTransition(0.0f);
+				}
+			}
+		}.run();
+	}	
+	
+	public abstract void animatorTransition(float percentComplete);
+	
+}
