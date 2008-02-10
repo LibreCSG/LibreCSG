cdc9c72b7ae3fcf2bd7004840fa1d503206b5517
whollyguacamole <whollyguacamole@bd7b2bbc-6a27-0410-ac9b-97bb208a358d>
2008-02-10 05:23:42 +0000

    documentation of what is envisioned for the material properties class.

--- trunk/backend/model/material/PartMaterial.java
+++ trunk/backend/model/material/PartMaterial.java
@@ -9,6 +9,16 @@ public class PartMaterial {
 	double b = 0.5;
 	double a = 0.5;
 	
+	// TODO: Until openGL implementation catch up to openGL2.0, I suggest we have 3 rendering levels:
+	// TODO: Render Level 1: r,g,b,a
+	// TODO: Render Level 2: ARB (assembly instruction) fragment program
+	// TODO: Render Level 3: openGL 2.0 GLSL perlin noise based procedural material shader.
+	
+	/**
+	 * The material out of which the part is made.  This should be 
+	 * dynamic enough to cover a full spectrum of real and creative 
+	 * material types. (wood, metal, glass, lava, zebra, leopard, etc.)
+	 */
 	public PartMaterial(double r, double g, double b, double a){
 		this.r = r;
 		this.g = g;
@@ -16,10 +26,19 @@ public class PartMaterial {
 		this.a = a;
 	}
 	
+	/**
+	 * setup the GL drawing configuration to correctly apply material in subsequent operations.
+	 * @param gl
+	 */
 	public void loadMaterial(GL gl){
 		gl.glColor4d(r, g, b, a);
 	}
 	
+	/**
+	 * dispose of any resources that may have been claimed from the loading of the material.
+	 * This should be called after the drawing of the part is complete.
+	 * @param gl
+	 */
 	public void disposeMaterial(GL gl){
 		
 	}
