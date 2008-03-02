6a75fd0aa603f09c9cd4bb88c30a20d647278976
whollyguacamole <whollyguacamole@bd7b2bbc-6a27-0410-ac9b-97bb208a358d>
2008-03-02 00:18:10 +0000

    revolve now actually works around a line, although there is still a large bug where sometimes the direction of the faces gets flipped, which in turn makes CSG boolean operations unhappy... but besides that, the "revolution" has begun! :)

--- trunk/ui/tools/build/ToolBuildRevolveModel.java
+++ trunk/ui/tools/build/ToolBuildRevolveModel.java
@@ -13,6 +13,7 @@ import backend.adt.SelectionList;
 import backend.adt.Translation3D;
 import backend.global.AvoGlobal;
 import backend.model.Build;
+import backend.model.Feature2D;
 import backend.model.Part;
 import backend.model.Sketch;
 import backend.model.CSG.BoolOp;
@@ -23,6 +24,7 @@ import backend.model.CSG.CSG_Solid;
 import backend.model.CSG.CSG_Vertex;
 import backend.model.ref.ModRef_Plane;
 import backend.model.sketch.Point2DList;
+import backend.model.sketch.Prim2D;
 import backend.model.sketch.Prim2DLine;
 import backend.model.sketch.Region2D;
 import backend.model.sketch.SketchPlane;
@@ -132,22 +134,41 @@ public class ToolBuildRevolveModel implements ToolModelBuild{
 				Double offset = paramSet.getParam("offset").getDataDouble();
 				SelectionList centerLineSel = paramSet.getParam("centerline").getDataSelectionList();
 				int faceCounter = 1;
-				if(selectionList != null && angle != null && offset != null && centerLineSel != null && selectionList.getSelectionSize() > 0 && centerLineSel.getSelectionSize()  > 0){
-					//System.out.println("drawing extrude at height=" + height + " and selection: " + selectionList.toString());
-					for(int i=0; i<selectionList.getSelectionSize(); i++){
-						Region2D includedRegion = sketch.getRegAtIndex(Integer.parseInt(selectionList.getStringAtIndex(i)));
-						if(includedRegion != null){
-							CSG_Solid newSolid =  getSolidFromRegion(includedRegion, angle, offset, null, feat2D3D.ID, faceCounter);
-							solid  = CSG_BooleanOperator.Union(solid, newSolid);
-							LinkedList<Region2D> cutRegions = includedRegion.getRegionsToCut();
-							for(Region2D cutReg : cutRegions){
-								if(cutReg != null){
-									CSG_Solid cutSolid = getSolidFromRegion(cutReg, angle, offset, null, feat2D3D.ID, faceCounter);
-									solid = CSG_BooleanOperator.Subtraction(solid, cutSolid);
-
+				// TODO: This is a CRAZY amount of nested IFs just to make sure the centerline is correct. 
+				//       Perhaps the ParamSet should include some functionality to alleviate this!
+				if(selectionList != null && angle != null && offset != null && 
+						centerLineSel != null && selectionList.getSelectionSize() > 0 && 
+						centerLineSel.getSelectionSize()  > 0){
+					String[] regNum = centerLineSel.getStringAtIndex(0).split("\\.");
+					if(regNum.length == 2 && sketch.getFeat2DListSize() > Integer.parseInt(regNum[0])){
+						Feature2D feat2D = sketch.getAtIndex(Integer.parseInt(regNum[0]));
+						if(feat2D != null && feat2D.prim2DList.size() > Integer.parseInt(regNum[1])){
+							Prim2D prim2DCLMaybe = feat2D.prim2DList.get(Integer.parseInt(regNum[1]));
+							if(prim2DCLMaybe instanceof Prim2DLine){
+								Prim2DLine prim2DCenterLine = (Prim2DLine)prim2DCLMaybe;
+								for(int i=0; i<selectionList.getSelectionSize(); i++){
+									Region2D includedRegion = sketch.getRegAtIndex(Integer.parseInt(selectionList.getStringAtIndex(i)));
+									
+									if(includedRegion != null){
+										CSG_Solid newSolid =  getSolidFromRegion(includedRegion, angle, offset, prim2DCenterLine, feat2D3D.ID, faceCounter);
+										solid  = CSG_BooleanOperator.Union(solid, newSolid);
+										LinkedList<Region2D> cutRegions = includedRegion.getRegionsToCut();
+										for(Region2D cutReg : cutRegions){
+											if(cutReg != null){
+												CSG_Solid cutSolid = getSolidFromRegion(cutReg, angle, offset, prim2DCenterLine, feat2D3D.ID, faceCounter);
+												solid = CSG_BooleanOperator.Subtraction(solid, cutSolid);
+											}
+										}
+									}
 								}
+							}else{
+								System.out.println("ToolBuilRevolveModel(getBuiltSolid): centerline was not a Prim2DLine!! aborting. ");
 							}
+						}else{
+							System.out.println("ToolBuilRevolveModel(getBuiltSolid): centerline prim2DList index out of range!! aborting. ");
 						}
+					}else{
+						System.out.println("ToolBuilRevolveModel(getBuiltSolid): centerline feature2D index out of range!! aborting. ");
 					}
 				}
 			}catch(Exception ex){
@@ -172,30 +193,82 @@ public class ToolBuildRevolveModel implements ToolModelBuild{
 		int angleSteps = (int)Math.max(Math.abs(angle)/10, 10);
 		double angleStep = radAngle/angleSteps;
 		CSG_Solid solid = new CSG_Solid();
+		double radOffAngle = offset * Math.PI / 180.0;
 
 		// bottom face
-		CSG_Face startFace = region.getCSG_Face();
-		if(angle > 0.0){ // make sure normal points correct way (outward).
-			startFace.flipFaceDirection();										
-		}
-		startFace.setIsSelectable(new ModRef_Plane(ID, faceCounter++));
-		solid.addFace(startFace);
+//		CSG_Face startFace = region.getCSG_Face();
+//		if(angle > 0.0){ // make sure normal points correct way (outward).
+//			startFace.flipFaceDirection();										
+//		}
+//		startFace.setIsSelectable(new ModRef_Plane(ID, faceCounter++));
+//		solid.addFace(startFace);
 
 		Point2DList pList = region.getPeremeterPointList();
 
 		if(pList.size() >= 3){
+			// assuming orientation relative to x-axis revolve...
+			
+			// find
+			double xdiff = centerline.getPtB().getX() - centerline.getPtA().getX();
+			double ydiff = centerline.getPtB().getY() - centerline.getPtA().getY();
+			
+			double xoffset = 0.0;
+			double yoffset = 0.0;
+			double zrot = 0.0;
+			if(ydiff == 0.0){
+				// line is parallel to x-axis, just run a yoffset!
+				yoffset = -centerline.getPtA().getY();
+			}else{
+				// line is not parallel to x-axis.
+				double slopeInv = xdiff / ydiff;
+				if(slopeInv == 0.0){
+					// line is vertical (parallel to y-axis)
+					System.out.println("Line is parallel to y-axis!");
+					xoffset = -centerline.getPtA().getX();
+					zrot = -Math.PI/2;
+				}else{
+					double clAngle = Math.atan(1/slopeInv);						
+					// okay, back to the basics here.. 
+					// for a line: y=mx+yintercept
+					double m = 1/slopeInv; // okay to do this since we know it is non-zero.
+					double yintercept = centerline.getPtA().getY() - m*centerline.getPtA().getX();
+					double xintercept = (0.0 - yintercept) / m;									
+					xoffset = -xintercept;
+					zrot = -clAngle;											
+				}
+			}			
+			
+			// now do the math to revolve the shell around the centerline.
+			CSG_Vertex preTrans = new CSG_Vertex(xoffset, yoffset, 0.0);
+			Rotation3D preRot = new Rotation3D(0.0, 0.0, zrot);
+			Translation3D postTrans = new Translation3D(-xoffset, -yoffset, 0.0);
+			Rotation3D postRot = new Rotation3D(0.0, 0.0, -zrot);
 			Translation3D noTrans3D = new Translation3D(0.0, 0.0, 0.0);
+			
+			// add first face...
+			CSG_Face startFace = region.getCSG_Face().getTranslatedCopy(preTrans);
+			startFace.applyTranslationRotation(noTrans3D, preRot);
+			startFace.applyTranslationRotation(noTrans3D, new Rotation3D(radOffAngle, 0.0, 0.0));
+			startFace.applyTranslationRotation(postTrans, postRot);
+			startFace.setIsSelectable(new ModRef_Plane(ID, faceCounter++));
+			if(angle <= 0.0){ // make sure normal points correct way (outward).
+				startFace.flipFaceDirection();										
+			}
+			startFace.setIsSelectable(new ModRef_Plane(ID, faceCounter++));
+			solid.addFace(startFace);
+			
+			// add all the side faces.
 			for(int i=0; i<angleSteps; i++){			
 				CSG_Vertex lastVert = new CSG_Vertex(pList.getLast(), 0.0);
-				CSG_Vertex curVert = new CSG_Vertex(pList.getLast(), 0.0);
+				CSG_Vertex curVert = new CSG_Vertex(pList.getLast(), 0.0).addToVertex(preTrans).getTranslatedRotatedCopy(noTrans3D, preRot);
 				Iterator<Point2D> pIter = pList.iterator();		
 				while(pIter.hasNext()){
 					lastVert = curVert;
-					curVert = new CSG_Vertex(pIter.next(), 0.0);					
-					CSG_Vertex a = lastVert.getTranslatedRotatedCopy(noTrans3D, new Rotation3D(offset+angleStep*i, 0.0, 0.0));
-					CSG_Vertex b = lastVert.getTranslatedRotatedCopy(noTrans3D, new Rotation3D(offset+angleStep*(i+1), 0.0, 0.0));
-					CSG_Vertex c = curVert.getTranslatedRotatedCopy(noTrans3D, new Rotation3D(offset+angleStep*(i+1), 0.0, 0.0));
-					CSG_Vertex d = curVert.getTranslatedRotatedCopy(noTrans3D, new Rotation3D(offset+angleStep*i, 0.0, 0.0));
+					curVert = new CSG_Vertex(pIter.next(), 0.0).addToVertex(preTrans).getTranslatedRotatedCopy(noTrans3D, preRot);				
+					CSG_Vertex a = lastVert.getTranslatedRotatedCopy(noTrans3D, new Rotation3D(radOffAngle+angleStep*i, 0.0, 0.0)).getTranslatedRotatedCopy(postTrans, postRot);
+					CSG_Vertex b = lastVert.getTranslatedRotatedCopy(noTrans3D, new Rotation3D(radOffAngle+angleStep*(i+1), 0.0, 0.0)).getTranslatedRotatedCopy(postTrans, postRot);
+					CSG_Vertex c = curVert.getTranslatedRotatedCopy(noTrans3D, new Rotation3D(radOffAngle+angleStep*(i+1), 0.0, 0.0)).getTranslatedRotatedCopy(postTrans, postRot);
+					CSG_Vertex d = curVert.getTranslatedRotatedCopy(noTrans3D, new Rotation3D(radOffAngle+angleStep*i, 0.0, 0.0)).getTranslatedRotatedCopy(postTrans, postRot);
 					CSG_Polygon newPoly = new CSG_Polygon(a, b, c, d);
 					if(angle < 0.0){
 						newPoly.reverseVertexOrder();
@@ -205,20 +278,21 @@ public class ToolBuildRevolveModel implements ToolModelBuild{
 						solid.addFace(newFace);
 					}
 				}
-			}			
+			}		
+			
+			// close it up! :)
+			CSG_Face endFace = region.getCSG_Face().getTranslatedCopy(preTrans);
+			endFace.applyTranslationRotation(noTrans3D, preRot);
+			endFace.applyTranslationRotation(noTrans3D, new Rotation3D(radOffAngle+radAngle, 0.0, 0.0));
+			endFace.applyTranslationRotation(postTrans, postRot);
+			endFace.setIsSelectable(new ModRef_Plane(ID, faceCounter++));
+			if(angle >= 0.0){ // make sure normal points correct way (outward).
+				endFace.flipFaceDirection();
+			}
+			solid.addFace(endFace);
 		}
 
-
-		// TODO: closing face if not 360.0 degrees!
-		/* something similar to extrude.. but with rotation and translation.
-		CSG_Face topFace = region.getCSG_Face().getTranslatedCopy(new CSG_Vertex(0.0, 0.0, height));
-		if(angle >= 0.0){ // make sure normal points correct way (outward).
-			topFace.flipFaceDirection();
-		}
-		topFace.setIsSelectable(new ModRef_Plane(ID, faceCounter++));
-		solid.addFace(topFace);
-		 */
-
+		
 
 		return solid;
 	}
