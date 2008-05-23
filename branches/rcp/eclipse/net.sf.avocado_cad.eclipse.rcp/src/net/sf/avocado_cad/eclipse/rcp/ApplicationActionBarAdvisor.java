ee1d48a7e05b56f8e9ad43d7e8ef893f401bcd1c
ndsherman <ndsherman@bd7b2bbc-6a27-0410-ac9b-97bb208a358d>
2008-05-23 02:25:02 +0000

    Initial import.

---
+++ branches/rcp/eclipse/net.sf.avocado_cad.eclipse.rcp/src/net/sf/avocado_cad/eclipse/rcp/ApplicationActionBarAdvisor.java
@@ -0,0 +1,14 @@
+package net.sf.avocado_cad.eclipse.rcp;
+
+import org.eclipse.ui.application.ActionBarAdvisor;
+import org.eclipse.ui.application.IActionBarConfigurer;
+
+public class ApplicationActionBarAdvisor extends ActionBarAdvisor {
+
+    public ApplicationActionBarAdvisor(IActionBarConfigurer configurer) {
+        super(configurer);
+    }
+
+
+    
+}
